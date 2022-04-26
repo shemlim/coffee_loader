c67a8fab0254e740d3937e5a3ab93c5490d481ef
Shem Lim <shemlim80@gmail.com>
2022-04-26 20:35:19 +0800

    test

--- app.py
+++ app.py
@@ -21,11 +21,20 @@ app = Flask(__name__)
 
 
 def connection():
+    # For server
+    # conn = psycopg2.connect(
+    #     host="ec2-52-86-56-90.compute-1.amazonaws.com",
+    #     database="demc2tb1r0urm0",
+    #     user='mqhureykqyyrfb',
+    #     password='4203d70494bbdc600a37641ffc08bbfba4e48ce6549b1285c042e1c56c76030f')
+
+    # For Local
     conn = psycopg2.connect(
-        host="ec2-52-86-56-90.compute-1.amazonaws.com",
-        database="demc2tb1r0urm0",
-        user='mqhureykqyyrfb',
-        password='4203d70494bbdc600a37641ffc08bbfba4e48ce6549b1285c042e1c56c76030f')
+        host="localhost",
+        database="usersantuy",
+        user='postgres',
+        password='Shemlim12#')
+
     cur = conn.cursor()
 
     return [conn,cur]
@@ -45,7 +54,6 @@ def create_db():
                                             'order_date date DEFAULT CURRENT_TIMESTAMP);'
                                             )
             conn.commit()
-
             cur.close()
             conn.close()
         else:
@@ -73,6 +81,49 @@ def select_db(head=5):
     conn.close()
     return mobile_records
 
+def create_cat_menu():
+    conn,cur = connection()
+    cur.execute("select * from information_schema.tables where table_name=%s", ('order_menu',))
+
+    if bool(cur.rowcount) == False:
+        print("e",bool(cur.rowcount))
+        cur.execute('CREATE TABLE order_menu (menu_id serial PRIMARY KEY ,'
+                                                'menu_name varchar (150) NOT NULL,'
+                                                'menu_status int,'
+                                                'menu_date date DEFAULT CURRENT_TIMESTAMP);'
+                                                )
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    return cur
+
+def insert_cat_menu():
+    conn,cur = connection()
+    sql = """insert into order_menu(menu_name,menu_status) values('Latte',1);
+             insert into order_menu(menu_name,menu_status) values('Mocca',1);
+             insert into order_menu(menu_name,menu_status) values('Green Tea Latte',1);
+             insert into order_menu(menu_name,menu_status) values('Americano',1);
+             insert into order_menu(menu_name,menu_status) values('Cappucino',1);
+             insert into order_menu(menu_name,menu_status) values('Espresso',1);
+
+          """
+    cur.execute(sql)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def select_cat_menu():
+    conn,cur = connection()
+    sql = "select * from order_menu;"
+    cur.execute(sql)
+
+    mobile_records = cur.fetchall()
+    conn.commit()
+    cur.close()
+    conn.close()
+    return mobile_records
+
 logging.basicConfig(stream=sys.stderr)
 # @app.route('/')
 # def index():
@@ -80,9 +131,13 @@ logging.basicConfig(stream=sys.stderr)
 
 @app.route('/')
 def success():
+    # For order coffee
     # create_db()
     # insert_db()
-    # all_data = select_db()
+    # For menu catalog
+    create_cat_menu()
+    insert_cat_menu()
+    all_data = select_db()
 
 
     # return str(all_data)
@@ -90,12 +145,23 @@ def success():
 
 @app.route('/getcoffee/<name>',methods=['GET'])
 def coffee_getter(name):
-    
-    img_url = url_for('static', filename='upload_photo/coffee2.jpg')
-    img_url2 = url_for('static', filename='upload_photo/coffee1.jpg')
-    img_url3 = url_for('static', filename='upload_photo/coffee3.jpg')
+    all_menu = select_cat_menu()
 
-    return render_template("ordercoffee.html",name=name,img_url = img_url,img_url2 = img_url2,img_url3 = img_url3)
-    
+
+    # img_url = url_for('static', filename='upload_photo/coffee2.jpg')
+    # img_url2 = url_for('static', filename='upload_photo/coffee1.jpg')
+    # img_url3 = url_for('static', filename='upload_photo/coffee3.jpg')
+
+    d_cat_menu = dict()
+    for x in all_menu:
+        image = url_for('static', filename='upload_photo/{}.jpg'.format(x[1]))
+        d_cat_menu[x[1]] =  dict({'name':x[1],'status':x[2],"image":image})
+        
+
+    # return str(d_cat_menu)
+    return render_template("ordercoffee.html",menu_dict=d_cat_menu)
+
+
+
 if __name__ == '__main__':
     app.run()
