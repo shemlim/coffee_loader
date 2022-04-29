ad8503b527c1b330f9b0e79f4c88e865faf30840
Shem Lim <shemlim80@gmail.com>
2022-04-29 10:59:47 +0800

    test

--- create_db.py
+++ create_db.py
@@ -1,12 +1,18 @@
 import psycopg2
 from datetime import timedelta,datetime
 
+class db_date:
+    def __init__(self):
+        self.date_now = datetime.now() + timedelta(hours=8)
+        self.date = self.date_now.date
+        self.hour = self.date_now.hour
+
 ## For order transaction ##
 def insert_order(list_order,table_order,order_total,order_status,priority=1):
     conn,cur = connection()
     sql = """insert into order_transac(order_table,order_details,order_priority,order_total,order_status,order_date) 
             values({},'{}',{},{},{},'{}');
-          """.format(table_order,list_order,priority,order_total,order_status,datetime.now())
+          """.format(table_order,list_order,priority,order_total,order_status,db_date.date_now)
     cur.execute(sql)
     conn.commit()
     cur.close()
@@ -50,12 +56,14 @@ def connection():
 ## For order transaction ##
 def check_order_transac_list():
     conn,cur = connection()
-    min = datetime.now()
+    
+    min = db_date.date_now
+    dt_today = db_date.date
     if min.hour <= 17:
-        min = datetime.strftime(datetime.today(),"%Y-%m-%d 06:00:00")
+        min = datetime.strftime(dt_today,"%Y-%m-%d 06:00:00")
     else:
-        min = datetime.strftime(datetime.today(),"%Y-%m-%d 17:30:00")
-    max = datetime.strftime(datetime.today(),"%Y-%m-%d 23:59:59")
+        min = datetime.strftime(dt_today,"%Y-%m-%d 17:30:00")
+    max = datetime.strftime(dt_today,"%Y-%m-%d 23:59:59")
     
     sql = "select count(*) from order_transac where order_date >= '{}' and order_date <= '{}' and order_status = 1;"\
             .format(min,max)
@@ -70,12 +78,13 @@ def check_order_transac_list():
 def select_order_list(order_id=None,head=1):
     conn,cur = connection()
     sql = ''
-    min = datetime.now()
+    min = db_date.date_now
+    dt_today = db_date.date
     if min.hour <= 17:
-        min = datetime.strftime(datetime.today(),"%Y-%m-%d 06:00:00")
+        min = datetime.strftime(dt_today,"%Y-%m-%d 06:00:00")
     else:
-        min = datetime.strftime(datetime.today(),"%Y-%m-%d 17:30:00")
-    max = datetime.strftime(datetime.today(),"%Y-%m-%d 23:59:59")
+        min = datetime.strftime(dt_today,"%Y-%m-%d 17:30:00")
+    max = datetime.strftime(dt_today,"%Y-%m-%d 23:59:59")
     if order_id != None:
         sql = """select * from order_transac where order_id = {}"""\
             .format(order_id)
